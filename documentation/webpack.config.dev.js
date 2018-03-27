3e8f99f94210fb09c625589c47b2ac4ad989c84d
Antoni André <antoniandre.web@gmail.com>
2018-03-27 11:53:17 +1100

    Improve webpack config.

--- documentation/webpack.config.dev.js
+++ documentation/webpack.config.dev.js
@@ -4,13 +4,17 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 
+const srcDir = path.join(__dirname, 'src')
+const distDir = __dirname
+
 module.exports = {
+  // context: srcDir,
   entry: {
-    'app': './src/index.js'
+    'app': srcDir + '/index.js'
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].js',
+    path: distDir
     // To use with express server.
     // publicPath: '/'
   },
@@ -23,10 +27,10 @@ module.exports = {
     historyApiFallback: true
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    // new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html',
+      template: './index.html',
       inject: true
     }),
     new CopyWebpackPlugin([
@@ -36,9 +40,12 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    modules: [
+      srcDir,
+      'node_modules'
+    ],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue: 'vue/dist/vue.esm.js'
     }
   },
   module: {
@@ -49,7 +56,7 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.scss$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { test: /\.(png|svg|jpe?g|gif|ico)$/, use: ['file-loader'] },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
     ]
   }
--- documentation/webpack.config.prod.js
+++ documentation/webpack.config.prod.js
@@ -5,46 +5,44 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-const srcDir = path.join(__dirname, 'src')
-const distDir = __dirname
-
 module.exports = {
   entry: {
     app: './src/index.js'
     // Also a good practice to separate third-party libs into a vendor chunk
-    // (less likely to change than local source code).
-    // vendor: [
-    //     'lodash'
-    // ]
+    // (less likely to change than local source code):
+    // vendor: ['lodash']
   },
   output: {
-    filename: '[name].js', // Hash is different on each build, allows smart caching/flushing of file.
-    path: distDir,
+    filename: '[name].[hash:8].js', // Hash is different on each build, allows smart caching/flushing of file.
+    path: path.resolve(__dirname, 'dist')
     // To use with express server.
     // publicPath: './'
   },
   devtool: 'source-map',
   devServer: {
-    // contentBase: './dist',
-    contentBase: distDir,
+    contentBase: './dist',
     host: '0.0.0.0',
     port: 5555,
     hot: true,
     historyApiFallback: true
   },
   plugins: [
-    // new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: '../index.html',
       template: './src/index.html',
-      inject: true
+      inject: true,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
     }),
     new CopyWebpackPlugin([
       'static'
     ]),
     new UglifyJSPlugin({ sourceMap: true }),
 
-    // Allow to do sth like:
+    // Allow the use of environment variables. E.g:
     // if (process.env.NODE_ENV !== 'production') {console.log('dev mode!')}
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
 
@@ -52,33 +50,19 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({ name: 'common' }) // Specify the common bundle's name.
   ],
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue: 'vue/dist/vue.min.js'
     }
   },
   module: {
     rules: [
       { test: /\.vue$/, loader: 'vue-loader' },
-      // { test: /\.html$/, loader: 'html-loader' },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            interpolate: true,
-            root: srcDir,
-            minimize: true,
-            collapseWhitespace: true,
-            removeComments: true
-          }
-        }
-      },
+      { test: /\.html$/, loader: 'html-loader' },
       { test: /\.pug$/, exclude: /node_modules/, use: ['html-loader', 'pug-html-loader'] },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.scss$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { test: /\.(png|svg|jpe?g|gif|ico)$/, use: ['file-loader?name=static/[name].[hash:8].[ext]'] },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
     ]
   }
