b16a92e908fdc0ac211a539c1bfb642e8259d479
Antoni André <antoniandre.web@gmail.com>
2018-07-16 23:07:51 +0800

    Cleanup.

--- documentation/src/index.js
+++ documentation/src/index.js
@@ -14,13 +14,6 @@ Vue.use(Vuetify, {
     primary: '#ff5252',
     secondary: '#42b983',
     maintext: '#999'
-    // Default Vuetify colors.
-    // secondary: '#424242',
-    // accent: '#82B1FF',
-    // error: '#FF5252',
-    // info: '#2196F3',
-    // success: '#4CAF50',
-    // warning: '#FFC107'
   }
 })
 
