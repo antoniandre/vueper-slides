a1a604a64e7cf3fc886bdf0cb333fee9269d4c57
Antoni André <antoniandre.web@gmail.com>
2018-07-16 22:02:43 +0800

    Update documentation about external css file.

--- documentation/src/pages/main/index.js
+++ documentation/src/pages/main/index.js
@@ -2,7 +2,7 @@ import { VueperSlides, VueperSlide } from '../../../../dist/vueperslides'
 import { precode } from '../../components/syntax-highlighter'
 import { highlight } from '../../components/highlight-message'
 
-import '../../../../dist/vueperslides.css'
+import '../../../../dist/vueperslides.min.css'
 import './style.scss'
 import './examples.scss'
 
