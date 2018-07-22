630613db121b28cd839ea86f98033fab37ea4a05
Antoni André <antoniandre.web@gmail.com>
2018-07-23 06:17:21 +0800

    Major webpack configs cleanup and improvement. :tada:

--- documentation/webpack.config.dev.js
+++ documentation/webpack.config.dev.js
@@ -1,71 +1,93 @@
 const webpack = require('webpack')
-const { getIfUtils, removeEmpty } = require('webpack-config-utils')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const nodeEnv = process.env.NODE_ENV || 'development'
-const { ifDevelopment, ifProduction } = getIfUtils(nodeEnv)
-const src = './src'
-const publicPath = '/vueper-slides/documentation/'
+const src = path.join(__dirname, 'src')
+const dist = path.join(__dirname, 'dist')
 
-module.exports = removeEmpty({
-  entry: src + '/index.js',
-
-  output: {
-    filename: ifProduction('[name]-bundle-[hash].js', '[name]-bundle.js'),
-    publicPath: publicPath
+module.exports = {
+  entry: {
+    app: src + '/index.js',
+    vendor: ['vue', 'vuetify', 'vue-router', 'vueperslides']
   },
-
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm.js'
-    }
+  output: {
+    filename: '[name].bundle.js',
+    path: dist,
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: dist,
+    host: 'vueperslides.test',
+    historyApiFallback: true,
+    hot: true
   },
 
   module: {
     rules: [
-      { test: /\.vue$/, loader: 'vue-loader' },
       { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.pug$/, exclude: /node_modules/, use: ['html-loader', 'pug-html-loader'] },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?cacheDirectory' },
-      { test: /\.s(a|c)ss$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpe?g|gif|ico)$/, use: ['file-loader?name=images/[name].[hash:8].[ext]'] },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+      { test: /\.pug$/, use: ['html-loader', 'pug-html-loader'] },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      // Extract SCSS / CSS.
+      {
+        test: /\.scss$/,
+        include: src,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true, sourceMap: true } },
+            // 'postcss-loader',// Used for auto vendor-prefixing - not needed on dev.
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { sourceMap: true } },
+            // 'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|svg#.*)$/,
+        use: { loader: 'file-loader', options: { name: '[name].[ext]' } }
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|ico|pdf)$/,
+        use: { loader: 'file-loader', options: { name: '[name].[ext]' } }
+      }
     ]
   },
 
-  devtool: ifDevelopment('eval-source-map', 'source-map'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: src + '/assets/images/favicon.png',
+      filename: 'index.html',
+      template: src + '/index.html'
+    }),
 
-  devServer: ifDevelopment({
-    host: '0.0.0.0',
-    port: 5555,
-    openPage: publicPath.substr(1),
-    contentBase: publicPath,
-    historyApiFallback: true,
-    hot: true
-  }),
+    // Extract all css into a proper file.
+    new ExtractTextPlugin('[name].css'),
 
-  plugins: removeEmpty([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(nodeEnv)
+    //Extract vendor CSS/JS
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        return module.context && module.context.indexOf('node_modules') !== -1
       }
     }),
 
-    new HtmlWebpackPlugin({
-      hash: true,
-      filename: 'index.html',
-      template: src + '/index.html',
-      environment: nodeEnv
-    }),
+    // new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
 
-    new CopyWebpackPlugin([src + '/assets']),
-
-    ifProduction(
-      new ExtractTextPlugin('[name]-bundle-[hash].css'),
-      new ExtractTextPlugin('[name]-bundle.css')
-    )
-  ])
-})
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  }
+}
--- documentation/webpack.config.prod.js
+++ documentation/webpack.config.prod.js
@@ -1,33 +1,62 @@
 const webpack = require('webpack')
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const src = path.join(__dirname, 'src')
-const dist = path.join(__dirname, 'dist')
+const dist = path.join(__dirname, '../docs')
 
 module.exports = {
-  entry: src + '/index.js',
+  entry: {
+    app: src + '/index.js',
+    vendor: ['vue', 'vuetify', 'vue-router', 'vueperslides']
+  },
   output: {
-    filename: '[name].[hash:8].js', // Hash is different on each build, allows smart caching/flushing of file.
-    path: dist
-    // To use with express server.
-    // publicPath: './'
+    filename: '[name].[hash:8].min.js', // Hash is different on each build, allows smart caching/flushing of file.
+    path: dist,
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: dist,
-    host: '0.0.0.0',
-    port: 5555,
-    historyApiFallback: true,
-    hot: true
+
+  module: {
+    rules: [
+      { test: /\.html$/, loader: 'html-loader' },
+      { test: /\.pug$/, use: ['html-loader', 'pug-html-loader'] },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      // Extract and minify SCSS / CSS.
+      {
+        test: /\.scss$/,
+        include: src,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true, sourceMap: true } },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true, sourceMap: true } },
+            'postcss-loader'
+          ]
+        })
+      },
+      { test: /\.(woff2?|eot|ttf|otf|svg#.*)$/, loader: 'file-loader' },
+      { test: /\.(png|svg|jpe?g|gif|ico|pdf)$/, loader: 'file-loader' }
+    ]
   },
+
   plugins: [
-    new CleanWebpackPlugin([dist]),
+    new CleanWebpackPlugin(dist),
     new HtmlWebpackPlugin({
-      filename: '../index.html',
+      favicon: src + '/assets/images/favicon.png',
+      filename: 'index.html',
       template: src + '/index.html',
       inject: true,
       minify: {
@@ -35,37 +64,30 @@ module.exports = {
         removeComments: true
       }
     }),
-    // new CopyWebpackPlugin([src + '/assets']),
+
+    // Extract all css into a proper file.
+    new ExtractTextPlugin('[name].css'),
+
+    //Extract vendor CSS/JS
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.min.js',
+      minChunks: Infinity
+    }),
+
+    // Deduplicate modules into a common bundle.
+    new webpack.optimize.CommonsChunkPlugin({ name: 'common' }), // Specify the common bundle's name.
+
     new UglifyJSPlugin({ sourceMap: true }),
 
     // Allow the use of environment variables. E.g:
     // if (process.env.NODE_ENV !== 'production') {console.log('dev mode!')}
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-
-    // Deduplicate modules.
-    new webpack.optimize.CommonsChunkPlugin({ name: 'common' }) // Specify the common bundle's name.
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') })
   ],
+
   resolve: {
     alias: {
       vue: 'vue/dist/vue.min.js'
     }
-  },
-  module: {
-    rules: [
-      { test: /\.vue$/, loader: 'vue-loader' },
-      { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.pug$/, exclude: /node_modules/, use: ['html-loader', 'pug-html-loader'] },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.scss$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.(png|svg|jpe?g|gif|ico)$/,
-        use: {
-          loader: 'file-loader',
-          options: { name: 'images/[name].[hash:8].[ext]', publicPath: './dist' }
-        }
-      },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
-    ]
   }
 }
