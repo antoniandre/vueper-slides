ea3d46606f1eab4a6a6ee59326e2e4832067a948
Antoni André <antoniandre.web@gmail.com>
2018-07-23 18:01:57 +0800

    Use v-icon for fontawesome 5 brands & cleanup.

--- documentation/src/index.js
+++ documentation/src/index.js
@@ -2,25 +2,22 @@ import Vue from 'vue'
 import router from './router'
 import Vuetify from 'vuetify'
 import '../node_modules/vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import './styles/index.scss'
 
-import fontawesome from '@fortawesome/fontawesome'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import brands from '@fortawesome/fontawesome-free-brands'
-fontawesome.library.add(brands)
-
 Vue.use(Vuetify, {
+  iconfont: 'fa',
   theme: {
     primary: '#ff5252',
     secondary: '#42b983',
-    maintext: '#999'
+    maintext: '#999',
+    lightgrey: '#eee'
   }
 })
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   router,
-  components: { FontAwesomeIcon },
   template: require('./template.pug'),
   data: () => ({
     offsetTop: 0,
