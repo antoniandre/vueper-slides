e9eb713150e227ee2fa653403ee69b6d20349a6e
Antoni André <antoniandre.web@gmail.com>
2018-08-17 12:04:18 +0800

    Update dependencies and build script. Vue is now a peer dependency.

--- build.js
+++ build.js
@@ -1,29 +1,31 @@
-const rollup = require('rollup')
-const resolve = require('rollup-plugin-node-resolve')
-const vue = require('rollup-plugin-vue')
-const babel = require('rollup-plugin-babel')
-const pug = require('rollup-plugin-pug')
+import { rollup, watch } from 'rollup'
+
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import vue from 'rollup-plugin-vue'
+import pug from 'rollup-plugin-pug'
 // const eslint = require('rollup-plugin-eslint')
-const uglify = require('rollup-plugin-uglify')
-const watch = require('rollup-watch')
-const postcss = require('rollup-plugin-postcss')
-const sass = require('rollup-plugin-sass')
-const autoprefixer = require('autoprefixer')
+import { uglify } from 'rollup-plugin-uglify'
+import postcss from 'rollup-plugin-postcss'
+import sass from 'rollup-plugin-sass'
+import autoprefixer from 'autoprefixer'
 
 const libName = 'vueperslides'
 const config = (min = false) => {
   return {
-    entry: 'src/index.js',
-    dest: `dist/${libName}${min ? '.min' : ''}.js`,
-    format: 'umd',
-    moduleName: 'VueperSlides',
-    sourceMap: true,
+    input: 'src/index.js',
+    output: {
+      file: `dist/${libName}${min ? '.min' : ''}.js`,
+      name: 'VueperSlides',
+      sourcemap: min,
+      format: 'umd'
+    },
     plugins: [
       resolve(),
       vue({ compileTemplate: true, css: true }),
       pug(),
       postcss({
-        extract: 'dist/vueperslides.min.css',
+        extract: `dist/${libName}.min.css`,
         plugins: [autoprefixer],
         // sourceMap: true,
         minimize: true
@@ -39,22 +41,28 @@ const stderr = console.error.bind(console)
 
 const eventHandler = (event, filename) => {
   switch (event.code) {
-    case 'STARTING':
+    case 'START':
       stderr('checking rollup-watch version...')
       break
-    case 'BUILD_START':
+    case 'BUNDLE_START':
       stderr(`bundling to ${filename}...`)
       break
-    case 'BUILD_END':
-      stderr(`${filename} bundled in ${event.duration}ms. Watching for changes...`)
+    case 'BUNDLE_END':
+      stderr(`${filename} bundled in ${event.duration}ms. Watching for changes..`)
+      break
+    case 'END':
+      // stderr(`Bundle done.`)
       break
     case 'ERROR':
-      stderr(`error: ${event.error}`)
+      stderr(`Error: ${event.error}`)
+      break
+    case 'FATAL':
+      stderr(`Fatal error: ${event.error}`)
       break
     default:
-      stderr(`unknown event: ${event}`)
+      stderr(`Unknown event: ${JSON.stringify(event, null, 2)}`)
   }
 }
 
-watch(rollup, config()).on('event', (event) => eventHandler(event, `dist/${libName}.js`))
-watch(rollup, config(true)).on('event', (event) => eventHandler(event, `dist/${libName}.min.js`))
+watch(config()).on('event', (event) => eventHandler(event, `dist/${libName}.js`))
+watch(config(true)).on('event', (event) => eventHandler(event, `dist/${libName}.min.js`))
