81f555ea3dc75c8b28c1bb32636c3147f8547a0d
Antoni Andre <antoniandre.web@gmail.com>
2020-05-30 14:00:26 +1000

    Update test case.

--- src/documentation/isolated-test-view.vue
+++ src/documentation/isolated-test-view.vue
@@ -1,37 +1,33 @@
 <template lang="pug">
 //- This is an isolated test view. Just for testing purpose.
 div
-  vueper-slides.no-shadow(
-    :slide-ratio="1 / 2"
-    lazy
-    lazy-load-on-drag
-    @image-loaded="log"
-    @image-failed="log")
+  vueper-slides.no-shadow(ref="vuperslides" :slide-ratio="1 / 2" autoplay)
     vueper-slide(
+      v-if="slides.length"
       v-for="(slide, i) in slides"
       :key="i"
       :title="`Slide ${i + 1}`"
       :content="`Content ${i + 1}.`"
       :image="slide.image")
-      template(v-slot:loader)
-        v-progress-circular(color="primary" indeterminate)
-        span.mt-3.primary--text.title Loading...
 </template>
 
 <script>
 import { VueperSlides, VueperSlide } from '@/components/vueperslides/'
 import '@/components/vueperslides/styles.scss'
 
+const allSlides = [
+  { image: 'https://farm4.staticflickr.com/3364/3409068082_bbecd0b7cc_o.jpg' },
+  { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Ramon_Covelo_Sakrisoy%20Dreams.jpg' },
+  { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Alex_Noriega_Pure_Magic.jpg' },
+  { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Jesse_Moran_Resurrect.jpg' },
+  { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Perez_Alonso%20Photography_Chocolate_Mountains.jpg' },
+  { image: require('@/assets/images/easton-wa-usa.jpg') }
+];
+
 export default {
   components: { VueperSlides, VueperSlide },
   data: () => ({
     slides: [
-      { image: 'https://farm4.staticflickr.com/3364/3409068082_bbecd0b7cc_o.jpg' },
-      { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Ramon_Covelo_Sakrisoy%20Dreams.jpg' },
-      { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Alex_Noriega_Pure_Magic.jpg' },
-      { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Jesse_Moran_Resurrect.jpg' },
-      { image: 'https://combo.staticflickr.com/ap/build/images/sohp/2019-top-25/Perez_Alonso%20Photography_Chocolate_Mountains.jpg' },
-      { image: require('@/assets/images/easton-wa-usa.jpg') }
     ],
     breakpoints: {
       700: {
@@ -46,6 +42,15 @@ export default {
       }
     }
   }),
+
+  mounted () {
+    // Test adding slides after mounted, 1 by 1 every 2 sec.
+    allSlides.forEach((slide, i) => {
+      setTimeout(() => {
+        this.slides.push(slide)
+      }, (i + 1)* 2000)
+    })
+  },
 
   methods: {
     log (param) {
