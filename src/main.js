b77de5111aa1d289b1f96f400c0035ec83949b09
Antoni Andre <antoniandre.web@gmail.com>
2020-06-22 20:39:50 +1000

    Serve fontawesome locally.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import 'regenerator-runtime/runtime'
 
 import Vue from 'vue'
 import vuetify from './plugins/vuetify'
+
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/brands.css'
+
 import App from './app'
 import router from './router/'
 
