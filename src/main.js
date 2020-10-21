2d3c1267967e65c3b0691f30f41bfe9fd6793f59
Antoni Andre <antoniandre.web@gmail.com>
2020-10-21 14:58:33 +1100

    Update vue-router and app root.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
-import Vue from 'vue'
-import vuetify from './plugins/vuetify'
+import { createApp, h } from 'vue'
+// import vuetify from './plugins/vuetify'
 
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
 import '@fortawesome/fontawesome-free/css/brands.css'
@@ -11,10 +11,7 @@ import '@fortawesome/fontawesome-free/css/brands.css'
 import App from './app'
 import router from './router/'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+createApp({
+  // vuetify,
+  render: () => h(App)
+}).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,16 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Documentation from '@/documentation/'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    component: Documentation
-  },
-  {
-    path: '/test',
-    component: () => import(/* webpackChunkName: "isolated-test-view" */ '@/documentation/isolated-test-view.vue')
-  }
-]
-
-export default new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Documentation
+    },
+    {
+      path: '/test',
+      component: () => import(/* webpackChunkName: "isolated-test-view" */ '@/documentation/isolated-test-view.vue')
+    }
+  ]
 })
