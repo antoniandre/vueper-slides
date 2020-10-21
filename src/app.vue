d19f094f04a60a392ad7a232f159cfeb659dd598
Antoni Andre <antoniandre.web@gmail.com>
2020-10-21 15:17:07 +1100

    Remove template 1st spaces & v-scroll directive.

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
-  v-app(:class="{ ready: ready }" v-scroll="onScroll")
-    v-container.px-0(fluid)
-      top-bar(:offset-top="offsetTop")
-      router-view
+v-app(:class="{ ready: ready }" :v-scroll="onScroll")
+  v-container.px-0(fluid)
+    top-bar(:offset-top="offsetTop")
+    router-view
 
-    v-fab-transition
-      v-btn(color="primary" fixed bottom right fab v-show="!goTopHidden" small v-scroll-to="'#top'")
-        //- Width to prevent ugly first load animation when icon is not yet ready.
-        v-icon(color="white" size="26" style="width: 24px") keyboard_arrow_up
+  v-fab-transition
+    v-btn(color="primary" fixed bottom right fab v-show="!goTopHidden" small :v-scroll-to="'#top'")
+      //- Width to prevent ugly first load animation when icon is not yet ready.
+      v-icon(color="white" size="26" style="width: 24px") keyboard_arrow_up
 
-    v-footer.px-0.py-2(color="white")
-      v-layout.max-widthed(wrap justify-center)
-        v-flex.xs12.sm6.text-center.text-sm-left.copyright.
-          Copyright © {{ (new Date()).getFullYear() }} Antoni André, all rights reserved.
-        v-flex.xs12.sm6.text-center.text-sm-right.made-with
-          .mb-1 This documentation is made with #[v-icon fab fa-vuejs], #[v-icon fab fa-html5], #[v-icon fab fa-css3], #[v-icon fab fa-sass] &amp; #[v-icon.heart favorite]
-          | View this project on #[a(href="https://github.com/antoniandre/vueper-slides" target="_blank") #[v-icon fab fa-github] Github].
+  v-footer.px-0.py-2(color="white")
+    v-layout.max-widthed(wrap justify-center)
+      v-flex.xs12.sm6.text-center.text-sm-left.copyright.
+        Copyright © {{ (new Date()).getFullYear() }} Antoni André, all rights reserved.
+      v-flex.xs12.sm6.text-center.text-sm-right.made-with
+        .mb-1 This documentation is made with #[v-icon fab fa-vuejs], #[v-icon fab fa-html5], #[v-icon fab fa-css3], #[v-icon fab fa-sass] &amp; #[v-icon.heart favorite]
+        | View this project on #[a(href="https://github.com/antoniandre/vueper-slides" target="_blank") #[v-icon fab fa-github] Github].
 </template>
 
 <script>
