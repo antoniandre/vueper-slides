34527b39dfe18708b99c56deda4aa552da79f672
Antoni Andre <antoniandre.web@gmail.com>
2020-10-23 19:30:34 +1100

    Cleanup.

--- src/components/vueperslides/index.js
+++ src/components/vueperslides/index.js
@@ -1,10 +1,4 @@
-import VueperSlides from './vueperslides'
-import VueperSlide from './vueperslide'
-
-// Expose component to global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('vueper-slides', VueperSlides)
-  window.Vue.component('vueper-slide', VueperSlide)
-}
+import VueperSlides from './vueperslides.vue'
+import VueperSlide from './vueperslide.vue'
 
 export { VueperSlides, VueperSlide }
