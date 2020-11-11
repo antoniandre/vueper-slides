dba05f64ad67165b16cce1d901ff5140b437e691
Antoni Andre <antoniandre.web@gmail.com>
2020-11-11 20:12:16 +1100

    Update the isolated test case.

--- src/documentation/isolated-test-view.vue
+++ src/documentation/isolated-test-view.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
 //- This is an isolated test view. Just for testing purpose.
 div
-  vueper-slides.no-shadow(:breakpoints="breakpoints" rtl)
-    vueper-slide(
+  vueper-slides.no-shadow(:breakpoints="breakpoints" bullets-outside :dragging-distance="50")
+    vueper-slide.black(
       v-if="slides.length"
       v-for="(slide, i) in slides"
       :key="i"
       :title="`${slide.title} - slide #${i + 1}`"
-      :image="slide.image")
+      :image="slide.image"
+      :video="slide.video")
 </template>
 
 <script>
@@ -19,22 +20,38 @@ export default {
   data: () => ({
     slides: [
       {
-        title: 'El Teide Volcano, Spain',
-        content: 'Photo by Max Rive',
-        image: `${process.env.BASE_URL}images/el-teide-volcano-spain.jpg`,
-        link: 'https://www.maxrivephotography.com/index/C0000rU1RKCHdqwI/G0000X57AtIzuRX0/I0000Gvr9HqdtyXk'
+        title: 'Aurora Borealis',
+        image: 'https://i.ytimg.com/vi_webp/ehJg_OlcjpE/maxresdefault.webp',
+        video: {
+          url: 'https://www.youtube.com/embed/ehJg_OlcjpE?autoplay=1&controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
+          props: {
+            allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          }
+        }
       },
       {
-        title: 'Chernobyl, Ukraine',
-        content: 'Photo by Jesse Moran',
-        image: `${process.env.BASE_URL}images/chernobyl-ukraine.jpg`,
-        link: 'https://www.flickr.com/photos/jessemoran'
+        title: 'Fjords',
+        image: 'https://i.ytimg.com/vi/2sr9MGkkeks/maxresdefault.jpg',
+        video: {
+          url: 'https://www.youtube.com/embed/2sr9MGkkeks?autoplay=1&controls=0&fs=0&modestbranding=1&color=white&iv_load_policy=3&autohide=1&enablejsapi=1',
+          props: {
+            allow: 'autoplay'
+          },
+          pointerEvents: false
+        }
+      },
+      {
+        title: 'Flower',
+        image: `${process.env.BASE_URL}images/flower.jpg`,
+        video: {
+          webm: `${process.env.BASE_URL}images/flower.webm`,
+          mp4: `${process.env.BASE_URL}images/flower.mp4`
+        }
       },
       {
         title: 'Crater Lake, Oregon, USA',
         content: 'Photo by Jesse Moran',
-        image: `${process.env.BASE_URL}images/crater-lake-oregon-usa.jpg`,
-        link: 'https://flic.kr/p/2cxrCmp'
+        image: `${process.env.BASE_URL}images/crater-lake-oregon-usa.jpg`
       }
     ],
     breakpoints: {
@@ -69,5 +86,4 @@ export default {
 </script>
 
 <style lang="scss">
-html {direction: rtl;}
 </style>
